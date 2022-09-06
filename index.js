df87e990064faf79387b48681d4ec33da48b4af0
Sebastian Zapata <sebastian.zapata@geniussports.com>
2022-09-06 12:06:50 -0500

    cambiando a mocklist para que retorne 8 elemento el mock

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { MockList } = require('@graphql-tools/mock');
 
 const typeDefs = gql`
+  scalar Date
+
   type SkiDay {
     id: ID!
-    date: String!
+    date: Date!
     mountain: String!
     conditions: Conditions
   }
@@ -21,19 +24,35 @@ const typeDefs = gql`
 
   type Mutation{
       addDay(input: AddDayInput!):SkiDay
-      removeDay(id: ID!): SkiDay!
+      removeDay(id: ID!): RemoveDayPayload!
   }
 
   input AddDayInput{
-      date: String!
+      date: Date!
       mountain: String!
       conditions: Conditions
   }
+
+  type RemoveDayPayload {
+    day: SkiDay!
+    removed: Boolean
+    totalBefore: Int
+    totalAfter: Int
+  }
 `;
+
+const mocks ={
+  Date: () => "2022-04-01",
+  String: ()=> "Cool data",
+  Query: () => ({
+    allDays: () => new MockList(8)
+  })
+}
+
 
 const server = new ApolloServer({
   typeDefs,
-  mocks: true,
+  mocks: mocks,
 });
 
 server.listen().then(({ url }) => console.log(`server running at ${url}`));
